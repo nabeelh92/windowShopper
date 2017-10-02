962b44a1a12523ca4346440e1ef89f0afc280040
nabeelh92 <nabeelhuq@gmail.com>
2017-10-01 19:39:01 -0700

    added ui elements and custom class for text fields

---
+++ window-Shopper/window-Shopper/Views/CurrenctTxtField.swift
@@ -0,0 +1,28 @@
+//
+//  CurrenctTxtField.swift
+//  window-Shopper
+//
+//  Created by Nabeel Huq on 2017-10-01.
+//  Copyright © 2017 Nabeel Huq. All rights reserved.
+//
+
+import UIKit
+
+class CurrenctTxtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+        
+    }
+    
+}
