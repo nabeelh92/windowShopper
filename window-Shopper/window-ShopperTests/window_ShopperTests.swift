639b99218f49d4dd5880eb1c17aca1d8bfddf8ec
nabeelh92 <nabeelhuq@gmail.com>
2017-10-02 00:59:11 -0700

    added unit tests in project

---
+++ window-Shopper/window-ShopperTests/window_ShopperTests.swift
@@ -0,0 +1,35 @@
+//
+//  window_ShopperTests.swift
+//  window-ShopperTests
+//
+//  Created by Nabeel Huq on 2017-10-01.
+//  Copyright © 2017 Nabeel Huq. All rights reserved.
+//
+
+import XCTest
+
+class window_ShopperTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
